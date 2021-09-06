06cd2916c70c7e62dc2cb51479fd82b76132426f
it21045158 <87773356+it21045158@users.noreply.github.com>
2021-09-06 21:17:28 +0000

    Tutorial 6 Completed

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,15 @@
 #include "Student.h"
 #include <iostream>
-
+using namespace std;
 // Assign studentId and name
 Student::assignDetails() {
-  
+void Student::assignDetails(int sid, char sName[]) {
+studentId = sid;
+strcpy(name,sName);
 }
 
 // Display StudentId and Name
 Student::display() {
-  
+cout << studentId;
+cout << name;
 }
--- Student.h
+++ Student.h
@@ -1,9 +1,16 @@
 class Student {
   // private section
+  private:
+    int studentId;
+    char name[20];
+
   //    int studentId
   //    name <- 20 charcters
   
   // public section
+  public:
+    void assignDetails(int sid, char sName[]);
+    void display();
   //    assignDetails() method declaration
   //    display() method declaration
 };
